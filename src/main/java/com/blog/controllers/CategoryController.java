69f26d03276f9bbc7214aff0668cc947cd8dac6b
PranjaliBramh <bramhankarpranjali@gmail.com>
2025-05-22 16:48:41 +0530

    Added category Api End Points

--- src/main/java/com/blog/controllers/CategoryController.java
+++ src/main/java/com/blog/controllers/CategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,14 +38,14 @@ public class CategoryController {
 		return new ResponseEntity<CategoryDto>(createCategory, HttpStatus.CREATED);
 	}
 	
-	@PostMapping("/{catId}")
+	@PutMapping("/{catId}")
 	public ResponseEntity<CategoryDto> updateCategory(@Valid @RequestBody CategoryDto categoryDto, @PathVariable Integer catId ) {
 
 		CategoryDto updateCategory = this.categoryService.updatecategory(categoryDto, catId);
 
 		return new ResponseEntity<CategoryDto>(updateCategory, HttpStatus.OK);
 	}
-	
+	 
 
 	@DeleteMapping("/{catId}")
 	public ResponseEntity<ApiResponse> deletecategory(@PathVariable Integer catId) {
--- src/main/java/com/blog/payloads/CategoryDto.java
+++ src/main/java/com/blog/payloads/CategoryDto.java
@@ -1,8 +1,11 @@
 package com.blog.payloads;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Singular;
 
 @NoArgsConstructor
 @Getter
@@ -10,6 +13,11 @@ import lombok.Setter;
 public class CategoryDto {
 	
 	private Integer categotyId;
+	@NotBlank
+	@Size(min=4, message = "min size category title is 4")
 	private String categoryTitle;
+	
+	@NotBlank
+	@Size(min = 10,message = "min size category Description is 10")
 	private String categoryDescription;
 }
